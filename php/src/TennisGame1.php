7296db4aeda512ed3df1a45373580a00ed52c360
Thomas Talbot <talbot@zeturf.com>
2021-03-07 12:13:14 +0100

    add getScoreQualifier

--- php/src/TennisGame1.php
+++ php/src/TennisGame1.php
@@ -80,21 +80,23 @@ class TennisGame1 implements TennisGame
                 $score .= "-";
                 $tempScore = $this->m_score2;
             }
-            switch ($tempScore) {
-                case 0:
-                    $score .= "Love";
-                    break;
-                case 1:
-                    $score .= "Fifteen";
-                    break;
-                case 2:
-                    $score .= "Thirty";
-                    break;
-                case 3:
-                    $score .= "Forty";
-                    break;
-            }
+            $score .= $this->getScoreQualifier($tempScore);
         }
         return $score;
+    }
+
+    protected function getScoreQualifier(int $tempScore): string
+    {
+        switch ($tempScore) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
+        return '';
     }
 }
